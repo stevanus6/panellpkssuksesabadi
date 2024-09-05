a34496a570f07b5bd92995a173cbc61e37cc5343
ayooke97 <bakti12356@gmail.com>
2024-09-05 14:04:54 +0700

    #11 Fix - Desain tampilan Baru

---
+++ revisi/layout/main.php
@@ -0,0 +1,9 @@
+<img src="assets/undraw_website.svg" alt="welcome" srcset="">
+<div class="welcome mt-4">
+    <h2>Hallo Asep!</h2>
+    <p>Selamat datang di LPKS Sukses Abadi Jaya. Semoga harimu menyenangkan!</p>
+    <div class="button mt-3">
+        <a href="fdwsg" class="btn btn-primary m-1">TKW Singapore</a>
+        <a href="fdwhk" class="btn btn-secondary m-1">TKW Hongkong</a>
+    </div>
+</div>
---
+++ revisi/layout/sidebar.php
@@ -0,0 +1,9 @@
+<aside class="sidebar bg-primary bg-gradient">
+    <ul class="p-0">
+        <li>
+            <div class="brand">
+                <h1 class="text-white text-center fs-4 py-3">SUKSES ABADI JAYA</h1>
+            </div>   
+        </li>
+    </ul>
+</aside>
