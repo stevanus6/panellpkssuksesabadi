b95f0752bc00d84cf254b1f209577f5bae45e541
your_name <322210002@student.machung.ac.id>
2024-09-06 16:20:52 +0700

    6 | Kasbon PL dan Footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="d-flex justify-content-center sticky-footer p-4 bg-white ">
-    <span>Copyright &copy; Copy Right</span>
+    <span>Copyright &copy; <a href="https://www.digitalkreasigroup.com">PT. Digital Kreasi Group</a></span>
                 <!-- <div class="container my-auto">
                     <div class="copyright text-center my-auto">
                     </div>
